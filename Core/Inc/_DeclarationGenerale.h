f8bd31ae2d2022a661864d22f1768d1c675b0c6a
matisserheaumeviale-ux <matisse.rheaumeviale@gmail.com>
2026-04-20 16:42:57 -0400

    Nouveau design complet de AFLC Board V1
    Nouveau design complet de AFLC Board V1 avec bluepill

---
+++ Core/Inc/_DeclarationGenerale.h
@@ -0,0 +1,64 @@
+ 
+
+
+// ***************** FICHIER:  _DeclarationGenerale.H  ************************
+//
+//  DEFINITION GENERALE POUR UN PROJET
+//
+//  Application realisee pour la carte BLUEPILL STM32F103
+//
+//  AUTEUR : Francois Bouchard
+//  DATE CREATION :    2024-06-03         VERSION: 1.0
+//  DATE MODIFICATION: ****-**-**
+//
+// ****************************************************************************
+ 
+ #ifndef DECLARATIONGENERALEH
+   #define DECLARATIONGENERALEH
+
+   // *************************************************************************
+   //
+   //                            DEFINITION DE MOT CLE
+   //
+   // *************************************************************************
+   #define ACTIF   1              //
+   #define INACTIF 0              //
+   
+   #define PESE    0              // Pour les boutons peses a 0.
+   #define PASPESE 1
+
+   #define ON      1              //
+   #define OFF     0              //
+   #define VRAI    1              //
+   #define FAUX    0              //
+   #define TRUE    1              //
+   #define FALSE   0              //
+
+
+   // *************************************************************************
+   //
+   //                            DEFINITION DE TYPE
+   //
+   // *************************************************************************
+    
+   #ifndef NULL
+     #define NULL    0  
+   #endif
+
+     
+   #define UC unsigned char
+   #define UI unsigned int
+   #define US unsigned short int
+   #define USI unsigned short int
+
+   #define UINT16 US
+
+
+#endif // #ifndef _DECLARATIONGENERALEH
+
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+
+
